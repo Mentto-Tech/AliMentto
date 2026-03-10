b4f12b07a2782b1d2c6d2b2b02991a5c3716d8f3
luis <euluishenrique7@gmail.com>
2026-03-10 08:25:44 -0300

    melhoria css 3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -44,7 +44,7 @@ function Dashboard() {
           <strong>AliMentto</strong>
           <p>Controle de Almoços</p>
         </div>
-        <div style={{ display: 'flex', gap: '10px', marginLeft: 'auto' }}>
+        <div style={{ display: 'flex', gap: '10px', marginLeft: 'auto', marginRight: '30px'}}>
           <button 
             className='btn-logout' 
             onClick={() => navigate('/alterar-senha')} 
